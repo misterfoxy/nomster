6fea25bc333e5904c960e439737308974f779948
misterfoxy <michaelscottfox1@gmail.com>
2019-07-08 20:25:40 -0700

    added validation to places controller and place model

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,11 +8,17 @@ class PlacesController < ApplicationController
     def new
         @place = Place.new
     end
+    
 
     def create
-        current_user.places.create(place_params)
-        redirect_to root_path
+        @place = current_user.places.create(place_params)
+        if @place.valid?
+          redirect_to root_path
+        else
+          render :new, status: :unprocessable_entity
+        end
     end
+
 
     def show
         @place = Place.find(params[:id])
@@ -34,8 +40,12 @@ class PlacesController < ApplicationController
         end
 
         @place.update_attributes(place_params)
-
-        redirect_to root_path
+        
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
     end
 
     def destroy
